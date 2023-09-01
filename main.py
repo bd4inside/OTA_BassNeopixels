d1ddb5c23a17bf59b3d8934c7803ae75f60e6ea9
bd4inside <98431290+bd4inside@users.noreply.github.com>
2023-09-01 11:12:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+from ota import OTAUpdater
+from wifi_config import SSID, PASSWORD
+
+firmware_url = "https://raw.githubusercontent.com/bd4inside/OTA_BassNeopixels/"
+
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+
+ota_updater.download_and_install_update_if_available()
+
+print("Works")
